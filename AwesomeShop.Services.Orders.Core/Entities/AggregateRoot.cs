b904b4d163323e9729c68c072040727d1c17eda7
Renato Ramon <renatophoenix123@gmail.com>
2024-12-09 12:52:06 -0300

    "fix(core): update AggregateRoot with null check for events list - Added null check for _events in AddEvent method to initialize the list if it's null feat(core): create IOrderRepository interface - Added IOrderRepository with GetByIdAsync, AddAsync, and UpdateAsync methods for Order entity"

--- AwesomeShop.Services.Orders.Core/Entities/AggregateRoot.cs
+++ AwesomeShop.Services.Orders.Core/Entities/AggregateRoot.cs
@@ -8,12 +8,15 @@ namespace AwesomeShop.Services.Orders.Core.Entities
 {
     public class AggregateRoot : IEntityBase
     {
-        private readonly List<IDomainEvent> _events = new List<IDomainEvent>();
+        private  List<IDomainEvent> _events = new List<IDomainEvent>();
         public Guid Id {get; protected set;}
 
         public IEnumerable<IDomainEvent> Events => _events;
 
         protected void AddEvent(IDomainEvent @event){
+            if(_events == null)
+            _events = new List<IDomainEvent>();
+            
             _events.Add(@event);
         }
     }
---
+++ AwesomeShop.Services.Orders.Core/Repositories/IOrderRepository.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AwesomeShop.Services.Orders.Core.Entities;
+
+namespace AwesomeShop.Services.Orders.Core.Repositories
+{
+    public interface IOrderRepository
+    {
+        Task <Order> GetByIdAsync(Guid id);
+        Task AddAsync(Order order);
+        Task UpdateAsync(Order order);
+
+        
+    }
+}
