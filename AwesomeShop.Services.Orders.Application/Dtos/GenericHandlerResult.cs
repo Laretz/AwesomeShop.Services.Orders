abd89fd59ba3d872bf28ff5619d6c5eede374702
Renato Ramon <renatophoenix123@gmail.com>
2024-12-09 16:06:06 -0300

    feat(application): finalização da camada application com registro do MediatR e DTO de resultados
    - Adicionado método de extensão `AddHandlers` para registrar handlers do MediatR na coleção de serviços.
    - Criados os DTOs `GenericHandlerResult` e `ValidationObject` para padronizar respostas de handlers e resultados de validação.

--- AwesomeShop.Services.Orders.Application/Dtos/GenericHandlerResult.cs
+++ AwesomeShop.Services.Orders.Application/Dtos/GenericHandlerResult.cs
@@ -5,8 +5,30 @@ using System.Threading.Tasks;
 
 namespace AwesomeShop.Services.Orders.Application.Dtos
 {
-    public class OrderViewModel
+    public class GenericHandlerResult
     {
-        
+        public GenericHandlerResult(string message, object data, bool success, List<ValidationObject> validations)
+        {
+            Message = message;
+            Data = data;
+            Success = success;
+            Validations = validations;
+        }
+
+        public string Message { get; private set; }
+        public object Data { get; private set; }
+        public bool Success { get; private set; }
+        public List<ValidationObject> Validations { get; private set; }
+    }
+
+    public class ValidationObject {
+        public ValidationObject(string property, string message)
+        {
+            Property = property;
+            Message = message;
+        }
+
+        public string Property { get; private set; }
+        public string Message { get; private set; }
     }
 }
