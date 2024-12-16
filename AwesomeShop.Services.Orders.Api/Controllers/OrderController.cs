3c88c4a86e9e255dabf4541b5f54ead89b2e4ef0
Renato Ramon <renatophoenix123@gmail.com>
2024-12-16 10:46:19 -0300

    feat(api): implementação do controller de pedidos com métodos GET e POST
    - Criado o controller `OrderController` para gerenciar pedidos.
    - Implementados os métodos `Get` para consultar um pedido por ID e `Post` para criar um novo pedido.
    - Validação simples no método `Post` para garantir que o comando não seja nulo.

--- AwesomeShop.Services.Orders.Api/Controllers/OrderController.cs
+++ AwesomeShop.Services.Orders.Api/Controllers/OrderController.cs
@@ -1,3 +1,8 @@
+using System;
+using System.Threading.Tasks;
+using AwesomeShop.Services.Orders.Application.Commands;
+using AwesomeShop.Services.Orders.Application.Queries;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AwesomeShop.Services.Orders.Api.Controllers
@@ -6,6 +11,30 @@ namespace AwesomeShop.Services.Orders.Api.Controllers
     [ApiController]
     public class OrderController : ControllerBase
     {
-        
+        private readonly IMediator _mediator;   
+        public OrderController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(Guid id){
+            var query = new GetOrderById(id);
+
+            var result = await _mediator.Send(query);
+
+            if (result == null)
+                return NotFound();
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] AddOrder command)
+        {
+            var id = await _mediator.Send(command);
+
+            return CreatedAtAction(nameof(Get), new {id = id}, command);
+        }
     }
 }
