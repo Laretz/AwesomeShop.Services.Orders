0a50d5ecfcd6dcb0b9016be597bc959bf058db76
Renato Ramon <renatophoenix123@gmail.com>
2024-12-16 12:57:55 -0300

    feat(infrastructure): implementar repositório de pedidos com MongoDB - Adicionada configuração do MongoDB com `MongoDbOptions`. - Implementado registro do cliente MongoDB e banco de dados via injeção de dependências. - Criado método de extensão `AddMongo` para registrar a configuração MongoDB. - Implementado repositório `IOrderRepository` para gerenciar pedidos no MongoDB.

--- AwesomeShop.Services.Orders.Infrastructure/Persistence/MongoDbOptions.cs
+++ AwesomeShop.Services.Orders.Infrastructure/Persistence/MongoDbOptions.cs
@@ -5,8 +5,9 @@ using System.Threading.Tasks;
 
 namespace AwesomeShop.Services.Orders.Infrastructure.Persistence
 {
-    public class MongoDbOptions
+  public class MongoDbOptions
     {
-        
+        public string Database { get; set; }
+        public string ConnectionString { get; set; }
     }
 }
