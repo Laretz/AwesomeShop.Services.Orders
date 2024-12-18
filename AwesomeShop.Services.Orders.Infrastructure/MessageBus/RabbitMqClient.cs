5e0fa51d74d93209d85f86dc3c679f2e128d60f0
Renato Ramon <renatophoenix123@gmail.com>
2024-12-18 18:31:49 -0300

    feat(infrastructure): implementar cliente RabbitMq para envio de mensagens
    - Implementada a classe `RabbitMqClient` para publicação de mensagens no RabbitMQ.
    - Adicionado o RabbitMq aos services da API

--- AwesomeShop.Services.Orders.Infrastructure/MessageBus/RabbitMqClient.cs
+++ AwesomeShop.Services.Orders.Infrastructure/MessageBus/RabbitMqClient.cs
@@ -1,15 +1,34 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using RabbitMQ.Client;
 
 namespace AwesomeShop.Services.Orders.Infrastructure.MessageBus
 {
     public class RabbitMqClient : IMessageBusClient
     {
-        public void Publish(object message, string routingKey, string exchange)
+        private readonly IConnection _connection;
+        public RabbitMqClient(ProducerConnection producerConnection)
         {
-            throw new NotImplementedException();
+            _connection = producerConnection.Connection;
+        }
+        
+          public void Publish(object message, string routingKey, string exchange)
+        {
+            var channel = _connection.CreateModel();
+
+            var settings = new JsonSerializerSettings {
+                NullValueHandling = NullValueHandling.Ignore,
+                ContractResolver = new CamelCasePropertyNamesContractResolver()
+            };
+
+            var payload = JsonConvert.SerializeObject(message, settings);
+            var body = Encoding.UTF8.GetBytes(payload);
+
+            channel.ExchangeDeclare(exchange, "topic", true);
+            
+            channel.BasicPublish(exchange, routingKey, null, body);
         }
     }
 }
