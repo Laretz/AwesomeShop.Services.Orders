b0e796effbf8a6e960fff1114c16ece05de3cc6a
Renato Ramon <renatophoenix123@gmail.com>
2024-12-18 21:45:10 -0300

    fix(api): adiciona redirecionamento automático para o Swagger na raiz do servidor
    Agora, ao acessar a URL raiz ("/") do servidor, o usuário é redirecionado automaticamente para a interface do Swagger.

--- AwesomeShop.Services.Orders.Application/Extensions.cs
+++ AwesomeShop.Services.Orders.Application/Extensions.cs
@@ -1,4 +1,6 @@
 
+using System;
+using System.Text;
 using AwesomeShop.Services.Orders.Application.Commands;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
@@ -11,6 +13,30 @@ namespace AwesomeShop.Services.Orders.Application
 
             return services;
         }
+        
+        public static string ToDashCase(this string text)
+        {
+            if(text == null) {
+                throw new ArgumentNullException(nameof(text));
+            }
+            if(text.Length < 2) {
+                return text;
+            }
+            var sb = new StringBuilder();
+            sb.Append(char.ToLowerInvariant(text[0]));
+            for(int i = 1; i < text.Length; ++i) {
+                char c = text[i];
+                if(char.IsUpper(c)) {
+                    sb.Append('-');
+                    sb.Append(char.ToLowerInvariant(c));
+                } else {
+                    sb.Append(c);
+                }
+            }
 
+            Console.WriteLine($"ToDashCase: "+ sb.ToString());
+
+            return sb.ToString();
+        }
     }
 }
